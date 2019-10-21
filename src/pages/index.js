a6be91e48def4389f7153edf4ae9c4f2d6d30811
Jean Souza <jean.souza@vitta.me>
2019-10-21 18:31:02 -0300

    chore: base

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Link } from "gatsby"
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <p>What a world.</p>
+  </div>
+)
