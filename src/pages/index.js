512505ec89744d42b1da1e6d3d1b0766190b1f17
Jean Souza <jean.souza@vitta.me>
2019-10-21 18:42:14 -0300

    header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
+import Header from "../components/header"
 export default () => (
   <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
+    <Header></Header>
     <p>What a world.</p>
   </div>
 )
