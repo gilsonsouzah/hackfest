3c4d5300f0f3676b2348e100dee6d3fba3c413c0
Tacio Medeiros <tacio.medeiros@vitta.me>
2019-10-21 20:01:46 -0300

    [fix] Corrections on files

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,15 @@
-<<<<<<< HEAD
-import React from "react"
-import { Button } from "react-bootstrap"
+import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export default () => (
   <div class="header">
     <div class="col-md-6">
       <a href="/">Hacktober Fest 2019 by bitta</a>
       <h1>
-        Apoie nossos projetos <a href="#">open source</a> e ganhe uma camiseta
-        exclusiva
+        Apoie nossos projetos <a href="#">open source</a> e ganhe uma camiseta exclusiva
       </h1>
       <Button variant="outline-dark">Conheça os projetos</Button>
     </div>
     <div class="col-md-6">trocar</div>
   </div>
 )
-=======
-import React from 'react'
-export default () => <div>header</div>
->>>>>>> e97af3db8b860fcfbd1ea2071cb8363fd92ac5b1
