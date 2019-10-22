a65f287cbae7588bacd2d687dfe1caa2aa0bdf16
Jean Souza <jean.souza@vitta.me>
2019-10-22 19:39:33 -0300

    src

--- src/components/main/footer.js
+++ src/components/main/footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Footer = () => <footer>esse é o rodapé</footer>
+const Footer = () => <footer></footer>
 
 export default Footer
--- src/components/main/header.js
+++ src/components/main/header.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Container, Row, Col } from 'react-bootstrap'
 
 const Header = () => (
-  <header>
-    <a href={'/'}>Hacktober Fest 2019 Vitta</a>
-    <h1>
-      Apoie nossos projetos <a href={'/'}>open source</a> e ganhe uma camiseta exclusiva
-    </h1>
-    <Button onClick={() => alert(true)}>Show Alert</Button>
-  </header>
+  <Container>
+    <Row>
+      <Col>
+        <a class={'logo'} href={'/'}>
+          <span class="sr-only">Hacktober Fest 2019 Vitta</span>
+        </a>
+        <h1 class={'heroTitle'}>
+          Apoie nossos projetos <a href={'/'}>open source</a> e ganhe uma camiseta
+          exclusiva
+        </h1>
+        <Button class={'heroButton'} variant={'outline-dark'} onClick={() => alert(true)}>
+          conheça os projetos
+        </Button>
+      </Col>
+      <Col>imagem</Col>
+    </Row>
+  </Container>
 )
 
 export default Header
