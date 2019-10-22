4afcfe3cab12f654a79173372a689dd4a051f29c
Sami Henrique <sami.henrique@vitta.me>
2019-10-22 19:46:37 -0300

    - patter filename

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Header from './main/header'
-import Body from './main/body'
-import Footer from './main/footer'
+import Header from './main/Header'
+import Body from './main/Body'
+import Footer from './main/Footer'
 import Page from './main/Page'
 import ProjCard from './ProjCard'
 import Register from './Register'
